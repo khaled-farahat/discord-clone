c481f67f9cdc7eda427ff50bc4c75b6f55a3031c
khaled-farahat <84389471+khaled-farahat@users.noreply.github.com>
2023-10-02 19:27:21 +0300

    add manage server members
     kick user
     change role of user

--- components/scroll-area.tsx
+++ components/scroll-area.tsx
@@ -1,12 +1,19 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface ScrollAreaProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const ScrollArea = ({ children }: ScrollAreaProps) => {
+const ScrollArea = ({ children, className }: ScrollAreaProps) => {
   return (
-    <div className="hide-scrollbar flex w-full flex-1 flex-col items-start overflow-y-auto">
+    <div
+      className={cn(
+        "hide-scrollbar flex w-full flex-1 flex-col items-start overflow-y-auto",
+        className,
+      )}
+    >
       {children}
     </div>
   );
